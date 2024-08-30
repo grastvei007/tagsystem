71e527df988eea5dafa6cebfa071f02f0f53d453
Stein Gunnar Grastveit <grastvei@pvv.org>
2024-08-30 20:01:30 +0200

    Refactor tag table model

--- taglisttablemodel.h
+++ taglisttablemodel.h
@@ -44,7 +44,6 @@ public:
     bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
     Qt::ItemFlags flags(const QModelIndex &index) const override;
 
-    bool insertRows(int row, int count, const QModelIndex &parent = QModelIndex()) override;
 private slots:
     void onTagCreated(int index);
     void onTagValueChanged(int index);
