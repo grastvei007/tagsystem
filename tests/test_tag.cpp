4506135059bc7f099e24faff142b134c3e9f757f
Stein Gunnar Grastveit <grastvei@pvv.org>
2025-10-28 18:01:03 +0100

    Link with lib tagsystem
    A simple tag test to verify linking.

--- tests/test_tag.cpp
+++ tests/test_tag.cpp
@@ -1,6 +1,28 @@
 #include <gtest/gtest.h>
 
-TEST(TestTag, test)
+#include <QString>
+
+#include <tag.h>
+#include <taglist.h>
+
+TEST(TestTag, createTag)
 {
-    EXPECT_TRUE(true);
+    QString subsystem("testsubsystem");
+    QString name("testname");
+    Tag::Type type = Tag::eInt;
+    int value = 10;
+    QString desciption("description");
+    auto *tag = TagList::sGetInstance().createTag(subsystem, name, type, value, desciption);
+
+    EXPECT_EQ(subsystem, tag->getSubsystem());
+    EXPECT_EQ(name, tag->getName());
+    EXPECT_EQ(value, tag->getIntValue());
+    EXPECT_EQ(desciption, tag->getDescription());
+
+    EXPECT_EQ(tag->getDoubleValue(), 0.0);
+    EXPECT_EQ(tag->getBoolValue(), false);
+    EXPECT_TRUE(tag->getStringValue().isEmpty());
+
+    // new tag should be updated
+    EXPECT_TRUE(tag->isUpdated());
 }
